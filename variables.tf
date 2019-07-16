5574300a5b80b48e4a99344f946be2482bd4b604
Ivan Serebryakov <i1831500@gmail.com>
2019-07-17 01:03:43 +0300

    keypair var deleted

--- variables.tf
+++ variables.tf
@@ -23,11 +23,6 @@ variable "accessing_computer_ip" {
   description = "Public IP of the computer accessing the cluster via kubectl or browser."
 }
 
-variable "keypair_name" {
-  type        = "string"
-  description = "Keypair file used to access instances via SSH"
-}
-
 variable "cluster_name" {
   type        = "string"
   description = "K8S cluester name"
