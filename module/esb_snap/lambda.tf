79e8a1ffbb0ae7d6a1841823263b62ef44050507
Serebryakov Ivan Igorevich <i.i.serebryakov@tinkoff.ru>
2019-07-16 20:08:01 +0300

    wip

--- module/esb_snap/lambda.tf
+++ module/esb_snap/lambda.tf
@@ -1,8 +1,8 @@
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "ebs_snapshot" {
   filename         = "lambda_function.zip"
-  function_name    = "esb_snapshot"
+  function_name    = "EBSSnapshot"
   role             = aws_iam_role.lambda.arn
-  handler          = "index.handler"
+  handler          = "main"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "go.1.13"
 }
